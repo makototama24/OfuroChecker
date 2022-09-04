46b09206eb4e5985a5c2433a1dcef07cb19b95c0
tama <ta195936ma.magazine@gmail.com>
2022-09-04 14:56:03 +0900

    表面レイアウト決定

---
+++ public/index.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // const loadEl = document.querySelector('#load');
+
+    // firebase.storage().ref('stamp1.png').getDownloadURL().then((url) => {
+    //   document.getElementById("img").src = url;
+    // });
+    // // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+    // // // The Firebase SDK is initialized and available here!
+    // //
+    // // firebase.auth().onAuthStateChanged(user => { });
+    // // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
+    // // firebase.firestore().doc('/foo/bar').get().then(() => { });
+    // // firebase.functions().httpsCallable('yourFunction')().then(() => { });
+    // // firebase.messaging().requestPermission().then(() => { });
+    // // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
+    // // firebase.analytics(); // call to activate
+    // // firebase.analytics().logEvent('tutorial_completed');
+    // // firebase.performance(); // call to activate
+    // //
+    // // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+
+    // try {
+    //   let app = firebase.app();
+    //   let features = [
+    //     'auth', 
+    //     'database', 
+    //     'firestore',
+    //     'functions',
+    //     'messaging', 
+    //     'storage', 
+    //     'analytics', 
+    //     'remoteConfig',
+    //     'performance',
+    //   ].filter(feature => typeof app[feature] === 'function');
+    //   loadEl.textContent = `Firebase SDK loaded with ${features.join(', ')}`;
+    // } catch (e) {
+    //   console.error(e);
+    //   loadEl.textContent = 'Error loading the Firebase SDK, check the console.';
+    // }
+
+    document.querySelector('.card').addEventListener("click", function(){
+        document.querySelector(".card-front").classList.toggle("reverse");
+        document.querySelector(".card-back").classList.toggle("reverse");
+      });
+  });
